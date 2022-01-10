5fea1b7b5a60c95663d16470c22db24c1c9e67b6
EDWIN CORREA ARIAS <edwincorrea28236@correo.itm.edu.co>
2022-01-09 23:06:58 -0500

    Add VehicleType

---
+++ API.Vehicles/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using API.Vehicles.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Vehicles.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<VehicleType> VehicleTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<VehicleType>().HasIndex(x => x.Description).IsUnique();
+        }
+
+    }
+}
---
+++ API.Vehicles/Migrations/20220110035232_AddTableVehicleTypev2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Vehicles.Migrations
+{
+    public partial class AddTableVehicleTypev2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
