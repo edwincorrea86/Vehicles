1e00ad50d8e3e0fd014486e13c513e94caaa52ff
EDWIN CORREA ARIAS <edwincorrea28236@correo.itm.edu.co>
2022-01-11 23:12:02 -0500

    Complete crud Procedures

--- API.Vehicles/Data/DataContext.cs
+++ API.Vehicles/Data/DataContext.cs
@@ -15,11 +15,13 @@ namespace API.Vehicles.Data
         }
 
         public DbSet<VehicleType> VehicleTypes { get; set; }
+        public DbSet<Procedure> Procedures { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<VehicleType>().HasIndex(x => x.Description).IsUnique();
+            modelBuilder.Entity<Procedure>().HasIndex(x => x.Description).IsUnique();
         }
 
     }
