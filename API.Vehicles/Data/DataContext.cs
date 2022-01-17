c2f6fb3f3c079ef6924d940cf2a2d8d79fddb0db
EDWIN CORREA ARIAS <edwincorrea28236@correo.itm.edu.co>
2022-01-16 21:54:46 -0500

    Complete Crud DocumentTypes

--- API.Vehicles/Data/DataContext.cs
+++ API.Vehicles/Data/DataContext.cs
@@ -14,6 +14,7 @@ namespace API.Vehicles.Data
         {
         }
 
+        public DbSet<DocumentType> DocumentTypes { get; set; }
         public DbSet<Brand> Brands { get; set; }
         public DbSet<VehicleType> VehicleTypes { get; set; }
         public DbSet<Procedure> Procedures { get; set; }
@@ -24,6 +25,7 @@ namespace API.Vehicles.Data
             modelBuilder.Entity<VehicleType>().HasIndex(x => x.Description).IsUnique();
             modelBuilder.Entity<Procedure>().HasIndex(x => x.Description).IsUnique();
             modelBuilder.Entity<Brand>().HasIndex(x => x.Description).IsUnique();
+            modelBuilder.Entity<DocumentType>().HasIndex(x => x.Description).IsUnique();
         }
 
     }
